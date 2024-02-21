fad86f65bcb43dc36af8f22edd70606b0d96b14d
Frederik Brauer Nissen - Bærbar <fred145a@edu.zealand.dk>
2024-02-21 11:13:08 +0100

    Done

--- UnitTests/BeerTests.cs
+++ UnitTests/BeerTests.cs
@@ -1,11 +1,44 @@
-﻿namespace UnitTests
+﻿using OO1_2;
+
+namespace UnitTests
 {
     [TestClass]
     public class BeerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestValidateName()
         {
+            Beer beer = new Beer(0, "Tuborg", 4.6);
+            Assert.IsTrue(beer.ValidateName());
+
+            Beer beer2 = new Beer(1, null, 4.7);
+            Assert.IsFalse(beer2.ValidateName());
+
+            Beer beer3 = new Beer(2, "GG", 4.6);
+            Assert.IsFalse(beer3.ValidateName());
+        }
+
+        [TestMethod]
+        public void TestValidatePrice()
+        {
+            Beer beer = new Beer(0, "Tuborg", 4.6);
+            Assert.IsTrue(beer.ValidateAlcoholByValoume());
+
+            Beer beer2 = new Beer(1, "Tuborg", -100);
+            Assert.IsFalse(beer2.ValidateAlcoholByValoume());
+
+            Beer beer3 = new Beer(2, "Tuborg", 8000);
+            Assert.IsFalse(beer3.ValidateAlcoholByValoume());
+
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Beer beer = new Beer(0, "Tuborg", 4.6);
+
+            Assert.IsInstanceOfType(beer.ToString(), typeof(string));
+            Assert.AreEqual("Id: 0, Name: Tuborg, Abv: 4,6", beer.ToString());
         }
     }
 }
--- UnitTests/BeersRepositoryTests.cs
+++ UnitTests/BeersRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OO1_2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,78 @@ namespace UnitTests
     {
 
         [TestMethod]
-        public void TestMethod()
+        public void Get()
         {
+            BeersRepository beers = new BeersRepository();
+            List<Beer> copyBeers = beers.Get();
+            Assert.IsNotNull(copyBeers);
+        }
 
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            BeersRepository beers = new BeersRepository();
+            Beer? newBeer = beers.GetById(1);
+
+            Console.WriteLine(newBeer);
+            Assert.IsNotNull(newBeer);
+            Assert.AreEqual(1, newBeer.Id);
+
+            Beer? newBeer2 = beers.GetById(100);
+            Console.WriteLine(newBeer2);
+            Assert.IsNull(newBeer2);
+        }
+
+        [TestMethod]
+        public void AddBeerTest()
+        {
+            BeersRepository beers = new BeersRepository();
+            int countBefore = beers.Get().Count;
+            beers.AddBeer(new Beer(1, "Albani", 24));
+            int countAfter = beers.Get().Count;
+
+            Console.WriteLine("Before: " + countBefore + ". After: " + countAfter);
+            Assert.AreNotEqual(countBefore, countAfter);
+        }
+
+        [TestMethod]
+        public void DeleteBeerTest()
+        {
+            BeersRepository beers = new BeersRepository();
+            int countBefore = beers.Get().Count;
+            beers.RemoveBeer(1);
+            int countAfter = beers.Get().Count;
+
+            Assert.AreNotEqual(countBefore, countAfter);
+
+            Assert.IsNull(beers.RemoveBeer(100));
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            BeersRepository beers = new BeersRepository();
+
+            Beer? beerBefore = beers.GetById(2);
+
+            beers.Update(2, new Beer(1, "Stærk øl", 22));
+
+            Beer? beerAfter = beers.GetById(2);
+
+            Console.WriteLine("Before: " + beerBefore + ". After: " + beerAfter);
+            Assert.AreNotEqual(beerBefore, beerAfter);
+
+            Assert.IsNull(beers.Update(100, new Beer(2, "Frede Tuborg", 6.7)));
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            BeersRepository beers = new BeersRepository();
+
+            Assert.IsInstanceOfType(beers.ToString(), typeof(string));
+            Assert.AreEqual("Id: 0, Name: Carlsberg, Abv: 4,6, Id: 1, Name: Tuborg, Abv: 4,5, Id: 2, " +
+                "Name: Guinnes, Abv: 5,2", beers.ToString());
         }
     }
 }
